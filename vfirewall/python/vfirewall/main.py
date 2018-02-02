4a142f57d770764f7234c794b2ea4ce556796c3e
ansible <ansible@nso.t-i.demo>
2018-02-02 20:29:50 +0100

    cleaned up vfirewall code

--- vfirewall/python/vfirewall/main.py
+++ vfirewall/python/vfirewall/main.py
@@ -1,33 +1,33 @@
 # -*- mode: python; python-indent: 4 -*-
-import ncs
-from ncs.application import Service
-
-# import some modules to caculate subnet
+# import two exttra package for netmask calculation
 import socket
 import struct
+
+import ncs
+from ncs.application import Service
 
 # ------------------------
 # SERVICE CALLBACK EXAMPLE
 # ------------------------
 class ServiceCallbacks(Service):
-
-    def _convert_to_wildcard(self, mask): 
-      # _ private functin
-      # Could use python3 ipaddress module in future
-      mask_bytes = struct.unpack('BBBB', socket.inet_aton(mask))
-      inverted_bytes = struct.pack('BBBB', *[~x & 0xFF for x in mask_bytes])
-      return socket.inet_ntoa(inverted_bytes)
+    def _convert_to_wildcard(self, mask):
+        # _ private functin
+        # Could use python3 ipaddress module in future
+        mask_bytes = struct.unpack('BBBB', socket.inet_aton(mask))
+        inverted_bytes = struct.pack('BBBB', *[~x & 0xFF for x in mask_bytes])
+        return socket.inet_ntoa(inverted_bytes)
 
     def _build_acl_rule(self, rule):
-      # turn our different leafs into a single string
-      # going to use .format in stead of simple +
-      # format: "permit ip 10.2.2.0 0.0.0.255 10.3.3.0 0.0.0.255 log"
-      # for this poc only IP supported but this can easly be expanded with more options.
-      acl_rule = "{0} ip {1} {2} {3} {4} log".format(rule.action,rule.src_ip, \
-                                              self._convert_to_wildcard(rule.src_mask),rule.dest_ip, \
-                                              self._convert_to_wildcard(rule.dest_mask))
-      self.log.info("ACL rule created=", acl_rule)
-      return acl_rule
+        # turn our different leafs into a single string
+        # going to use .format in stead of simple +
+        # format: "permit ip 10.2.2.0 0.0.0.255 10.3.3.0 0.0.0.255 log"
+        # for this poc only IP supported but this can easly be expanded with more options.
+        acl_rule = "{0} ip {1} {2} {3} {4} log".format(rule.action, rule.src_ip, \
+                                            self._convert_to_wildcard(rule.src_mask), \
+                                            rule.dest_ip, \
+                                            self._convert_to_wildcard(rule.dest_mask))
+        self.log.info("ACL rule created=", acl_rule)
+        return acl_rule
 
     # The create() callback is invoked inside NCS FASTMAP and
     # must always exist.
@@ -38,19 +38,19 @@ class ServiceCallbacks(Service):
         # we can't use ncs.template.Variables to add a list of ACL rules
         # we need to the apply template for every rule
         for rule in service.access_list_rules:
-          self.log.info('processing rule=', rule)
+            self.log.info('processing rule=', rule)
 
-          # create template object to simplify passing of variables when applying a template.
-          vars = ncs.template.Variables()
-          vars.add('DEVICE', service.device)
-          vars.add('ACL_NAME', service.name)
-          vars.add('ACL_RULE', self._build_acl_rule(rule))
-          
-          # so we have our vars, now apply to template object with our variable instance
-          template = ncs.template.Template(service)
-          template.apply('vfirewall-template', vars)
+            # create template object to simplify passing of variables when applying a template.
+            tvars = ncs.template.Variables()
+            tvars.add('DEVICE', service.device)
+            tvars.add('ACL_NAME', service.name)
+            tvars.add('ACL_RULE', self._build_acl_rule(rule))
 
-    # The pre_modification() and post_modification() callbacks are optional,
+            # so we have our vars, now apply to template object with our variable instance
+            template = ncs.template.Template(service)
+            template.apply('vfirewall-template', tvars)
+
+     # The pre_modification() and post_modification() callbacks are optional,
     # and are invoked outside FASTMAP. pre_modification() is invoked before
     # create, update, or delete of the service, as indicated by the enum
     # ncs_service_operation op parameter. Conversely
